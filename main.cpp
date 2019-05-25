24d482090c606f70076425d08a7cbfa8f026f462
Kyrylo Glamazdin <33227539+Kyrylo-Glamazdin@users.noreply.github.com>
2019-05-25 18:43:06 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,20 +3,17 @@
 int main(){
     
     PathFinder p;
-    
     p.readStationsInput("names_of_stations.csv");
     p.readConnectionsInput("connections.csv");
-    vector<Station*> result = p.findShortestPath("New Utrecht Av", "Sheepshead Bay");
+    vector<Station*> result = p.findShortestPath("Bay Pkwy: N", "Times Sq - 42 St: N,Q,R,S,W,1,2,3,7");
+    
+    cout << endl;
     
     for (int i = 0; i < result.size(); i++){
-        if (i < result.size() - 1){
-            cout << result[i]->getActualName() << " --> ";
-        }
-        else{
-            cout << result[i]->getActualName() << endl;
-        }
+        cout << result[i]->getActualName() << endl;
     }
     
+    cout << endl;
     
     p.delete_stations();
     
